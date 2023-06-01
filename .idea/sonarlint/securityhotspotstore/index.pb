14cc6e0b5e77c602d9e098fa3e2ed39a500ee860
DDidas <david.didas@aol.com>
2023-06-01 15:06:47 +0200

    refactoring

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,9 @@
+
+\
+,.idea/inspectionProfiles/Project_Default.xml,4/9/496a238a6afa168dbaf6efd37bb459331589579c
+L
+frontend/css/style-login.css,1/d/1d6c9e79373e79715f05ec7ad4fb2b844003f155
+O
+frontend/css/style-register.css,7/5/7512e7bedbbdf4c40234263df34c55a662070d66
+<
+database.sql,d/d/dd715606dbf1c1508d07bd20c1262a49c65d1863
--- backend/api.py
+++ backend/api.py
@@ -1,7 +1,11 @@
+import datetime
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 import sqlite3
+import csv
+from fastapi.responses import FileResponse
 
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -16,7 +20,7 @@ app = FastAPI()
 # Add middleware for CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["*", "/Users/daviddidas/Desktop/DLRG_KOMPRESSOR/frontend/html"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -40,7 +44,7 @@ async def create_user_endpoint(user_data: dict):
     conn = sqlite3.connect("../identifier.sqlite")
     zeiger = conn.cursor()
     zeiger.execute("INSERT INTO main.accounts VALUES (?,?,?,?)", (
-        user_data['id'], user_data['name'], user_data['guthaben'], user_data['admin']
+        user_data['id'], user_data['name'], 0, user_data['admin']
     ))
     conn.commit()
     zeiger.close()
@@ -95,3 +99,44 @@ async def get_guthaben_by_id_endpoint(id: int):
     result = cursor.fetchone()
     conn.close()
     return {"guthaben": float(result[0])} if result else {"guthaben": None}
+
+
+import csv
+from fastapi.responses import FileResponse
+
+
+@app.get("/export_users_csv")
+async def export_users_csv_endpoint():
+    conn = sqlite3.connect('../identifier.sqlite')
+    cursor = conn.cursor()
+    cursor.execute("SELECT name, guthaben FROM accounts")
+    results = cursor.fetchall()
+    conn.close()
+
+    # Create a temporary file to store the CSV data
+    filename = "users.csv"
+    with open(filename, mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(["Name", "Guthaben"])  # Write header
+        writer.writerows(results)  # Write user data rows
+
+    # Return the CSV file as a download
+    return FileResponse(filename, filename="users.csv", media_type='text/csv')
+
+
+@app.get("/reset_guthaben")
+async def reset_guthaben_endpoint():
+    conn = sqlite3.connect('../identifier.sqlite')
+    cursor = conn.cursor()
+
+    # Create a new table with the name "abrechnung_" + current date
+    today = datetime.datetime.today()
+    table_name = "abrechnung_" + today.strftime("%Y%m%d%H%M%S")
+    cursor.execute(f"CREATE TABLE {table_name} AS SELECT * FROM main.accounts")
+
+    # Set guthaben of all users to 0
+    cursor.execute("UPDATE main.accounts SET guthaben = 0")
+
+    conn.commit()
+    conn.close()
+    return {"message": f"Guthaben reset and table {table_name} created successfully"}
